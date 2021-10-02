bb2f57ea397c3dd4dce6176856c0dd49c6cc2ec0
Skarlaks71 <mapisaro07@gmail.com>
2021-10-01 22:44:58 -0300

    finish helix curve

---
+++ Math Projections/Assets/Scripts/CurveMaker.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CurveMaker : MonoBehaviour
+{
+
+    public Transform point;
+
+    public float speed;
+    public float radius;
+    public float b;
+
+    [Header("Radius Controller")]
+    public float limitRadius = 15f;
+    public float speedRadius = 2f;
+
+    [Header("Color Controller")]
+    public Color currentColor;
+    public Color desireColor;
+    public float speedTransition = 2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentColor = GetComponentInChildren<TrailRenderer>().startColor;
+
+        StartCoroutine("HelixCurve");
+        StartCoroutine("WaitForAnim", 2f);
+        StartCoroutine("ColorTransition");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.R))
+        {
+            StartCoroutine("AnimRadius");
+        }
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            StopCoroutine("AnimRadius");
+        }
+    }
+    IEnumerator WaitForAnim(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        StartCoroutine("AnimRadius");
+    }
+    IEnumerator HelixCurve()
+    {
+        float timeS = 0;
+        while (true)
+        {
+            float t = timeS * Mathf.PI / 2;
+            point.position = CalculateHelixCurve(t, radius, b);
+            yield return new WaitForSeconds(.01f);
+            timeS += speed * Time.deltaTime; 
+            /*if(timeS > 30f)
+            {
+                timeS = 0;
+            }*/
+        }
+        
+    }
+
+    IEnumerator AnimRadius()
+    {
+        bool stopIncrease = false;
+        while (radius > 0)
+        {
+            if (radius < limitRadius && stopIncrease == false)
+            {
+                radius += speedRadius * Time.deltaTime;
+                if (radius > limitRadius) stopIncrease = true;
+            }
+            else
+            {
+                if (radius < 0.01f)
+                {
+                    radius = 0;
+                }
+                else
+                {
+                    radius -= speedRadius * Time.deltaTime;
+                }
+                
+            }
+            yield return new WaitForSeconds(.01f);
+        }
+        
+    }
+
+    IEnumerator ColorTransition()
+    {
+        float timeT = 0;
+        while (point.position.y < 270f)
+        {
+            GetComponentInChildren<TrailRenderer>().startColor = Color.Lerp(currentColor, desireColor, timeT);
+            yield return new WaitForSeconds(.1f);
+            timeT += Time.deltaTime / speedTransition;
+        }
+    }
+
+    Vector3 CalculateHelixCurve(float t, float a, float b)
+    {
+        Vector3 r = new Vector3(a * Mathf.Cos(t), b * t, a * Mathf.Sin(t));
+        return r;
+    }
+
+    
+}
---
+++ Math Projections/Assets/Scripts/FollowPoint.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPoint : MonoBehaviour
+{
+    public Transform point;
+    public float height;
+    public Color nightColor;
+    public float speedTransition = 0.05f;
+
+    float dstToPoint;
+    Color currentColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dstToPoint  = (point.position - transform.position).magnitude;
+        currentColor = GetComponent<Camera>().backgroundColor;
+        StartCoroutine("NightTransition");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(point.position.y < 283f)
+            transform.position = new Vector3(transform.position.x, point.position.y + dstToPoint - height, transform.position.z);
+
+        
+
+    }
+    IEnumerator NightTransition()
+    {
+        float timeT = 0;
+        while (point.position.y < 270f)
+        {
+            GetComponent<Camera>().backgroundColor = Color.Lerp(currentColor,nightColor,  timeT);
+            yield return new WaitForSeconds(.1f);
+            timeT += Time.deltaTime / speedTransition;
+        }
+    }
+}
